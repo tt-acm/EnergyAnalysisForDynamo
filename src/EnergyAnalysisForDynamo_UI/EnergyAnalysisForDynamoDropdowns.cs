c33a22402cdba23e0649b77841b744e724a2d906
Ben Howes <bhowes@thorntontomasetti.com>
2015-05-13 12:41:52 -0400

    Dropdowns working in 0.8.0
    Progress on #94

--- src/EnergyAnalysisForDynamo_UI/EnergyAnalysisForDynamoDropdowns.cs
+++ src/EnergyAnalysisForDynamo_UI/EnergyAnalysisForDynamoDropdowns.cs
@@ -12,16 +12,30 @@ using ProtoCore.AST.AssociativeAST;
 
 namespace EnergyAnalysisForDynamo_UI
 {
-
+    /// <summary>
+    /// Middleman class for creating dropdown nodes in EA4D, using enums as inputs.
+    /// </summary>
     public abstract class EA4DDropdownBase : DSDropDownBase
     {
+        /// <summary>
+        /// Constructor for this abstract class.  Pass in the name of the node, and the enum to convert
+        /// </summary>
+        /// <param name="value">The name of the node</param>
+        /// <param name="e">The enum to populate the dropdown list with</param>
         public EA4DDropdownBase(string value, Enum e) : base(value) 
         {
             stringsFromEnum(e);
         }
 
+        /// <summary>
+        /// A local variable to store the list of strings representing the enum
+        /// </summary>
         private List<string> myDropdownItems = new List<string>();
 
+        /// <summary>
+        /// Populate our local list of strings using the enum that was passed into the constructor
+        /// </summary>
+        /// <param name="e"></param>
         private void stringsFromEnum(Enum e) 
         {
             foreach (var i in Enum.GetValues(e.GetType()))
@@ -30,6 +44,9 @@ namespace EnergyAnalysisForDynamo_UI
             }
         }
 
+        /// <summary>
+        /// The populate items override.  Not sure why this gets called before the constructor, but it does!
+        /// </summary>
         public override void PopulateItems()
         {
             Items.Clear();
@@ -39,6 +56,11 @@ namespace EnergyAnalysisForDynamo_UI
             }
         }
 
+        /// <summary>
+        /// No clue what this does.  I found an example here and modified it: https://github.com/DynamoDS/DynamoRevit/blob/Revit2015/src/Libraries/RevitNodesUI/RevitDropDown.cs
+        /// </summary>
+        /// <param name="inputAstNodes"></param>
+        /// <returns></returns>
         public override IEnumerable<AssociativeNode> BuildOutputAst(List<AssociativeNode> inputAstNodes)
         {
             if (Items.Count == 0)
@@ -56,88 +78,88 @@ namespace EnergyAnalysisForDynamo_UI
     [IsDesignScriptCompatible]
     public class BuildingTypeDropdown : EA4DDropdownBase
     {
-        public BuildingTypeDropdown() : base("Building Type", new gbXMLBuildingType()) { }
+        public BuildingTypeDropdown() : base(">", new gbXMLBuildingType()) { }
     }
 
     [NodeName("HVAC System Type Dropdown")]
     [NodeCategory("EnergyAnalysisForDynamo.EnergySettings")]
     [NodeDescription("Select a HVAC System type to use with the GBSforDynamo Energy Settings node.")]
     [IsDesignScriptCompatible]
-    public class HVACtypeDropdown : EnumAsString<gbXMLBuildingHVACSystem>
+    public class HVACtypeDropdown : EA4DDropdownBase
     {
-        public HVACtypeDropdown(WorkspaceModel workspace) : base() { }
+        public HVACtypeDropdown() : base(">", new gbXMLBuildingHVACSystem()) { }
     }
 
     [NodeName("Operating Schedules Dropdown")]
     [NodeCategory("EnergyAnalysisForDynamo.EnergySettings")]
     [NodeDescription("Select an Operating Schedule to use with the Energy Settings node.")]
     [IsDesignScriptCompatible]
-    public class OperatingSchedulesDropdown : EnumAsString<gbXMLBuildingOperatingSchedule>
+    public class OperatingSchedulesDropdown : EA4DDropdownBase
     {
-        public OperatingSchedulesDropdown(WorkspaceModel workspace) : base() { }
+        public OperatingSchedulesDropdown() : base(">", new gbXMLBuildingOperatingSchedule()) { }
     }
 
     [NodeName("Conceptual Wall Construction Types Dropdown")]
     [NodeCategory("EnergyAnalysisForDynamo.EnergySettings")]
     [NodeDescription("Select a Conceptual Construction Type to use with the Set Surface Parameters node.")]
     [IsDesignScriptCompatible]
-    public class ConcWallConstTypeDropdown : EnumAsString<ConceptualConstructionWallType>
+    public class ConcWallConstTypeDropdown : EA4DDropdownBase
     {
-        public ConcWallConstTypeDropdown(WorkspaceModel workspace) : base() { }
+        public ConcWallConstTypeDropdown() : base(">", new ConceptualConstructionWallType()) { }
     }
     
     [NodeName("Conceptual Glazing Construction Types Dropdown")]
     [NodeCategory("EnergyAnalysisForDynamo.EnergySettings")]
     [NodeDescription("Select a Conceptual Construction Type to use with the Set Surface Parameters node.")]
     [IsDesignScriptCompatible]
-    public class ConcGlazingConstTypeDropdown : EnumAsString<ConceptualConstructionWindowSkylightType>
+    public class ConcGlazingConstTypeDropdown : EA4DDropdownBase
     {
-        public ConcGlazingConstTypeDropdown(WorkspaceModel workspace) : base() { }
+        public ConcGlazingConstTypeDropdown() : base(">", new ConceptualConstructionWindowSkylightType()) { }
     }
 
     [NodeName("Conceptual Floor Construction Types Dropdown")]
     [NodeCategory("EnergyAnalysisForDynamo.EnergySettings")]
     [NodeDescription("Select a Conceptual Construction Type to use with the Set Surface Parameters node.")]
     [IsDesignScriptCompatible]
-    public class ConcFloorConstTypeDropdown : EnumAsString<ConceptualConstructionFloorSlabType>
+    public class ConcFloorConstTypeDropdown : EA4DDropdownBase
     {
-        public ConcFloorConstTypeDropdown(WorkspaceModel workspace) : base() { }
+        public ConcFloorConstTypeDropdown() : base(">", new ConceptualConstructionFloorSlabType()) { }
     }
 
     [NodeName("Conceptual Roof Construction Types Dropdown")]
     [NodeCategory("EnergyAnalysisForDynamo.EnergySettings")]
     [NodeDescription("Select a Conceptual Construction Type to use with the Set Surface Parameters node.")]
     [IsDesignScriptCompatible]
-    public class ConcRoofConstTypeDropdown : EnumAsString<ConceptualConstructionRoofType>
+    public class ConcRoofConstTypeDropdown : EA4DDropdownBase
     {
-        public ConcRoofConstTypeDropdown(WorkspaceModel workspace) : base() { }
+        public ConcRoofConstTypeDropdown() : base(">", new ConceptualConstructionRoofType()) { }
     }
 
     [NodeName("Space Types Dropdown")]
     [NodeCategory("EnergyAnalysisForDynamo.EnergySettings")]
     [NodeDescription("Select a Space Type to use with the Set Zone Parameters node.")]
     [IsDesignScriptCompatible]
-    public class SpaceTypeDropdown : EnumAsString<gbXMLSpaceType>
+    public class SpaceTypeDropdown : EA4DDropdownBase
     {
-        public SpaceTypeDropdown(WorkspaceModel workspace) : base() { }
+        public SpaceTypeDropdown() : base(">", new gbXMLSpaceType()) { }
     }
 
     [NodeName("Condition Types Dropdown")]
     [NodeCategory("EnergyAnalysisForDynamo.EnergySettings")]
     [NodeDescription("Select a Condition Type to use with the Zone Parameters node.")]
     [IsDesignScriptCompatible]
-    public class ConditionType : EnumAsString<gbXMLConditionType>
+    public class ConditionType : EA4DDropdownBase
     {
-        public ConditionType(WorkspaceModel workspace) : base() { }
+        public ConditionType() : base(">", new gbXMLConditionType()) { }
     }
 
     [NodeName("Energy Data File Types Dropdown")]
     [NodeCategory("EnergyAnalysisForDynamo.GetAnalysisResults")]
     [NodeDescription("Select a Energy Data Type to use with Get Energy Model Files Node.")]
     [IsDesignScriptCompatible]
-    public class EnergyDataFileTypes : EnumAsString<EnergyDataFileType>
+    public class EnergyDataFileTypes : EA4DDropdownBase
     {
-        public EnergyDataFileTypes(WorkspaceModel workspace) : base() { }
+        public EnergyDataFileTypes() : base(">", new EnergyDataFileType()) { }
     }
 
     public enum EnergyDataFileType
