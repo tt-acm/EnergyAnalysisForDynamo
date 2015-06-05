61cd7a01a292d65b212424e7f143e8ba52966b8b
Ian Keough <ian.keough@autodesk.com>
2015-06-05 15:11:23 -0700

    Add a startup script.

---
+++ src/StartEnergyAnalysisForDynamo_Debug.bat
@@ -0,0 +1,4 @@
+set DYNAMO_API=C:\Users\Ian\Documents\GitHub\Dynamo\bin\AnyCPU\Debug
+set REVIT_API=C:\Program Files\Autodesk\Revit Architecture 2014
+set REVIT_VERSION=Revit_2014
+"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe" "EnergyAnalysisForDynamo.sln"
