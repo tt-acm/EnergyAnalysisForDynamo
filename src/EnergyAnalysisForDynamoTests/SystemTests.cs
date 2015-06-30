d9ad2b8b59aa9981491d7ff5a5eb4b3a71ade4f7
Ben Howes <bhowes@thorntontomasetti.com>
2015-06-30 14:15:40 -0400

    Updated system tests
    to all be pass fail.  Could be more sophisticated - need some more
    time...

--- src/EnergyAnalysisForDynamoTests/SystemTests.cs
+++ src/EnergyAnalysisForDynamoTests/SystemTests.cs
@@ -35,41 +35,16 @@ namespace EnergyAnalysisForDynamoTests
         {
             //open and run the example file
             OpenAndRunDynamoDefinition(@".\EnergyAnalysisForDynamo_ex1a_SetProjectEnergySettings.dyn");
-            
-
-            //check to see that the value[s] that we set with Dynamo actually took in Revit.
-
-            //Revit energy settings object
-            var es = Autodesk.Revit.DB.Analysis.EnergyDataSettings.GetFromDocument(DocumentManager.Instance.CurrentUIDocument.Document);
-
-
-            //glazing percentage
-            var myTargetGlazingPercentage = (double)GetPreviewValue("83f5eb3b-234f-4081-8461-bd1af9ae6708");
-            if (myTargetGlazingPercentage != es.PercentageGlazing) Assert.Fail();
-
-            //shade depth
-            var myTargetShadeDepth = (double)GetPreviewValue("d4373a50-6b22-49ae-a392-90a2ab77148a");
-            if (myTargetShadeDepth != es.ShadeDepth) Assert.Fail();
-
-            //skylight percentage
-            var myTargetSkylightPercentage = (double)GetPreviewValue("68e4d7c7-bdb7-419a-a77d-17a8850e486f");
-            if (myTargetSkylightPercentage != es.PercentageSkylights) Assert.Fail();
-
-            //HVAC system type --- fix dropdowns first
-
-            //Operating Schedule --- fix dropdowns first
-
-            //Core offset amount
-            var myTargetCoreOFfset = (double)GetPreviewValue("8972da79-c508-4dd2-ab56-6301cd4e5128");
-            if (myTargetCoreOFfset != es.MassZoneCoreOffset) Assert.Fail();
-
-            //Dividie perimeter
-            var myTargetDividePerimeter = (bool)GetPreviewValue("8da9e555-e7e4-4143-9b50-ef9b2dd8f069");
-            if (myTargetDividePerimeter != es.MassZoneDividePerimeter) Assert.Fail();
-
-
-            //if we got here, nothing failed.
-            Assert.Pass();
+            //check for errors and assert accordingly
+            string errString = CompileErrorsIntoString();
+            if (string.IsNullOrEmpty(errString))
+            {
+                Assert.Pass();
+            }
+            else
+            {
+                Assert.Fail(errString);
+            }
 
         }
 
@@ -81,36 +56,16 @@ namespace EnergyAnalysisForDynamoTests
         {
             //open and run the example file
             OpenAndRunDynamoDefinition(@".\EnergyAnalysisForDynamo_ex1b_CreateEnergyModelAndSetSurfaceParams.dyn");
-
-            ////get a handle on the surface we are setting properties on
-            //var mySurfaceID = (EnergyAnalysisForDynamo.ElementId)GetPreviewValue("98cb8c9e-8cdd-488a-9e8f-02fb9d84d721");
-            //var mySurface = (MassSurfaceData)DocumentManager.Instance.CurrentUIDocument.Document.GetElement(new Autodesk.Revit.DB.ElementId( mySurfaceID.InternalId));
-
-            //BH 20150616 - I couldn't get the above to work - could not get any preview values back from our nodes or from a code block.  
-            //Everything came back null.  ???   Hard coding for now...
-            var myWallSurface = (MassSurfaceData)DocumentManager.Instance.CurrentUIDocument.Document.GetElement(new Autodesk.Revit.DB.ElementId(205245));
-            //var myWindowSurface = (MassSurfaceData)DocumentManager.Instance.CurrentUIDocument.Document.GetElement(new Autodesk.Revit.DB.ElementId(205256));
-
-
-            //Glazing percentage
-            double myTargetGlazingPercentage = (double)GetPreviewValue("f4b472cb-7f0e-487d-8ff3-1b951d0a9f8b");
-            if (myTargetGlazingPercentage != myWallSurface.PercentageGlazing) Assert.Fail();
-
-            //Shading Depth
-            double myTargetShadingDepth = (double)GetPreviewValue("9299930a-564d-4996-9ef1-2ad42f4e86cd");
-            if (myTargetShadingDepth != myWallSurface.ShadeDepth) Assert.Fail();
-
-            //Sill Height
-            double myTargetSillHeight = (double)GetPreviewValue("f996a98e-44f0-44c0-9086-7ca30a646454");
-            if (myTargetSillHeight != myWallSurface.SillHeight) Assert.Fail();
-
-            //Wall Conceptual Construction --- fix dropdowns first.
-
-            //Window Conceptual Construction --- fix dropdowns first.
-
-
-            //if we got here, nothing failed.
-            Assert.Pass();
+            //check for errors and assert accordingly
+            string errString = CompileErrorsIntoString();
+            if (string.IsNullOrEmpty(errString))
+            {
+                Assert.Pass();
+            }
+            else
+            {
+                Assert.Fail(errString);
+            }
 
         }
 
@@ -122,17 +77,16 @@ namespace EnergyAnalysisForDynamoTests
         {
             //open and run the example file
             OpenAndRunDynamoDefinition(@".\EnergyAnalysisForDynamo_ex1c_CreateEnergyModelAndSetZoneParams.dyn");
-
-
-            //get a handle on the Revit zone we are setting properties on
-            var myZone = (MassZone)DocumentManager.Instance.CurrentUIDocument.Document.GetElement(new Autodesk.Revit.DB.ElementId(205231));
-
-            //space types --- fix dropdowns first 
-
-            //condition types --- fix dropdpwns first 
-
-            //if we got here, nothing failed.
-            Assert.Pass();
+            //check for errors and assert accordingly
+            string errString = CompileErrorsIntoString();
+            if (string.IsNullOrEmpty(errString))
+            {
+                Assert.Pass();
+            }
+            else
+            {
+                Assert.Fail(errString);
+            }
         }
 
         /// <summary>
@@ -143,6 +97,16 @@ namespace EnergyAnalysisForDynamoTests
         {
             //open and run the example file
             OpenAndRunDynamoDefinition(@".\EnergyAnalysisForDynamo_ex1d_iterativeAnalysisExample.dyn");
+            //check for errors and assert accordingly
+            string errString = CompileErrorsIntoString();
+            if (string.IsNullOrEmpty(errString))
+            {
+                Assert.Pass();
+            }
+            else
+            {
+                Assert.Fail(errString);
+            }
         }
 
         /// <summary>
@@ -153,12 +117,15 @@ namespace EnergyAnalysisForDynamoTests
         {
             //open and run the example file
             OpenAndRunDynamoDefinition(@".\EnergyAnalysisForDynamo_ex1e_SetSurfaceParamsByOrientation.dyn");
-            foreach (var i in AllNodes)
+            //check for errors and assert accordingly
+            string errString = CompileErrorsIntoString();
+            if (string.IsNullOrEmpty(errString))
             {
-                if (IsNodeInErrorOrWarningState(i.GUID.ToString()))
-                {
-                    Assert.Fail("The node called '" + i.NickName + "' failed or threw a warning.");
-                }
+                Assert.Pass();
+            }
+            else
+            {
+                Assert.Fail(errString);
             }
         }
 
@@ -170,12 +137,15 @@ namespace EnergyAnalysisForDynamoTests
         {
             //open and run the example file
             OpenAndRunDynamoDefinition(@".\EnergyAnalysisForDynamo_ex2a_DriveSurfacesByOrientation.dyn");
-            foreach (var i in AllNodes)
+            //check for errors and assert accordingly
+            string errString = CompileErrorsIntoString();
+            if (string.IsNullOrEmpty(errString))
             {
-                if (IsNodeInErrorOrWarningState(i.GUID.ToString()))
-                {
-                    Assert.Fail("The node called '" + i.NickName + "' failed or threw a warning.");
-                }
+                Assert.Pass();
+            }
+            else
+            {
+                Assert.Fail(errString);
             }
         }
 
@@ -187,12 +157,15 @@ namespace EnergyAnalysisForDynamoTests
         {
             //open and run the example file
             OpenAndRunDynamoDefinition(@".\EnergyAnalysisForDynamo_ex3a_CreategbXMLfromMass.dyn");
-            foreach (var i in AllNodes)
+            //check for errors and assert accordingly
+            string errString = CompileErrorsIntoString();
+            if (string.IsNullOrEmpty(errString))
             {
-                if (IsNodeInErrorOrWarningState(i.GUID.ToString()))
-                {
-                    Assert.Fail("The node called '" + i.NickName + "' failed or threw a warning.");
-                }
+                Assert.Pass();
+            }
+            else
+            {
+                Assert.Fail(errString);
             }
         }
 
@@ -204,12 +177,15 @@ namespace EnergyAnalysisForDynamoTests
         {
             //open and run the example file
             OpenAndRunDynamoDefinition(@".\EnergyAnalysisForDynamo_ex3b_CreategbXMLfromZones.dyn");
-            foreach (var i in AllNodes)
+            //check for errors and assert accordingly
+            string errString = CompileErrorsIntoString();
+            if (string.IsNullOrEmpty(errString))
             {
-                if (IsNodeInErrorOrWarningState(i.GUID.ToString()))
-                {
-                    Assert.Fail("The node called '" + i.NickName + "' failed or threw a warning.");
-                }
+                Assert.Pass();
+            }
+            else
+            {
+                Assert.Fail(errString);
             }
         }
 
@@ -221,12 +197,15 @@ namespace EnergyAnalysisForDynamoTests
         {
             //open and run the example file
             OpenAndRunDynamoDefinition(@".\EnergyAnalysisForDynamo_ex4a_CreateNewProjectAndGetProjectLists.dyn");
-            foreach (var i in AllNodes)
+            //check for errors and assert accordingly
+            string errString = CompileErrorsIntoString();
+            if (string.IsNullOrEmpty(errString))
             {
-                if (IsNodeInErrorOrWarningState(i.GUID.ToString()))
-                {
-                    Assert.Fail("The node called '" + i.NickName + "' failed or threw a warning.");
-                }
+                Assert.Pass();
+            }
+            else
+            {
+                Assert.Fail(errString);
             }
         }
 
@@ -238,12 +217,15 @@ namespace EnergyAnalysisForDynamoTests
         {
             //open and run the example file
             OpenAndRunDynamoDefinition(@".\EnergyAnalysisForDynamo_ex4b_UploadgbxmlAndCreateBaseRun.dyn");
-            foreach (var i in AllNodes)
+            //check for errors and assert accordingly
+            string errString = CompileErrorsIntoString();
+            if (string.IsNullOrEmpty(errString))
             {
-                if (IsNodeInErrorOrWarningState(i.GUID.ToString()))
-                {
-                    Assert.Fail("The node called '" + i.NickName + "' failed or threw a warning.");
-                }
+                Assert.Pass();
+            }
+            else
+            {
+                Assert.Fail(errString);
             }
         }
 
@@ -255,12 +237,15 @@ namespace EnergyAnalysisForDynamoTests
         {
             //open and run the example file
             OpenAndRunDynamoDefinition(@".\EnergyAnalysisForDynamo_ex5a_GetRunResultsSummary.dyn");
-            foreach (var i in AllNodes)
+            //check for errors and assert accordingly
+            string errString = CompileErrorsIntoString();
+            if (string.IsNullOrEmpty(errString))
             {
-                if (IsNodeInErrorOrWarningState(i.GUID.ToString()))
-                {
-                    Assert.Fail("The node called '" + i.NickName + "' failed or threw a warning.");
-                }
+                Assert.Pass();
+            }
+            else
+            {
+                Assert.Fail(errString);
             }
         }
 
@@ -272,13 +257,40 @@ namespace EnergyAnalysisForDynamoTests
         {
             //open and run the example file
             OpenAndRunDynamoDefinition(@".\EnergyAnalysisForDynamo_ex6_DownloadDetailedResults.dyn");
+            
+            //check for errors and assert accordingly
+            string errString = CompileErrorsIntoString();
+            if (string.IsNullOrEmpty(errString))
+            {
+                Assert.Pass();
+            }
+            else
+            {
+                Assert.Fail(errString);
+            }
+
+        }
+
+        /// <summary>
+        /// A utility function to loop over a sample file and list any nodes in error or warning state.
+        /// </summary>
+        /// <returns></returns>
+        private string CompileErrorsIntoString()
+        {
+            //a string to return
+            string errors = null;
+
+            //loop over the active collection of nodes.
             foreach (var i in AllNodes)
             {
                 if (IsNodeInErrorOrWarningState(i.GUID.ToString()))
                 {
-                    Assert.Fail("The node called '" + i.NickName + "' failed or threw a warning.");
+                    errors += "The node called '" + i.NickName + "' failed or threw a warning." + System.Environment.NewLine;
                 }
             }
+
+            //return the errors string
+            return errors;
         }
 
     }
